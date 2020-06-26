a87f103893af5d65b206385edf41e28b2fe841cd
hrojah <hrojah@gmail.com>
2020-06-26 20:22:06 +0500

    правки

--- js/java.js
+++ js/java.js
@@ -1,5 +1,8 @@
 var search = document.querySelector(".search-hotel");
 var searchForm = document.querySelector(".search-hotel-form");
+
 search.addEventListener("click", function (evt) {
-  searchForm.classList.toggle("hide-form");
-});
+
+    searchForm.classList.toggle("show-form");
+
+})
