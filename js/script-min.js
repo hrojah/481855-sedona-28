ead80034c5b3af532af3bcf0e2fc771d5c890176
hrojah <hrojah@gmail.com>
2020-07-04 15:18:40 +0500

    правки

---
+++ js/script-min.js
@@ -0,0 +1,1 @@
+var search=document.querySelector(".search-hotel");var searchForm=document.querySelector(".search-hotel-form");searchForm.classList.add("hide-form");search.addEventListener("click",function(evt){searchForm.classList.toggle("show-form")})
--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var search = document.querySelector(".search-hotel");
 var searchForm = document.querySelector(".search-hotel-form");
 
+searchForm.classList.add("hide-form");
+
 search.addEventListener("click", function (evt) {
 
     searchForm.classList.toggle("show-form");
-
 })
