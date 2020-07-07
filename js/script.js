af3db436feb6b692e3785b52e93d99da06d6855c
hrojah <hrojah@gmail.com>
2020-07-07 22:49:49 +0500

    правки

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 var search = document.querySelector(".search-hotel");
 var searchForm = document.querySelector(".search-hotel-form");
+var input = searchForm.querySelector(".hotel-input");
 
 searchForm.classList.add("hide-form");
 
 search.addEventListener("click", function (evt) {
 
     searchForm.classList.toggle("show-form");
+})
+
+searchForm.addEventListener("submit", function (evt) {
+
+    if (!input.value) {
+      evt.preventDefault();
+      searchForm.classList.add("error-form");
+    }
 })
